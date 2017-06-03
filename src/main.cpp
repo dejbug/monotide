8857799c0d1adba64a9ac482facc1469d7c4bfb0
Dejan Budimir <dejbug@users.noreply.github.com>
2017-06-03 15:11:13 +0200

    -

--- src/main.cpp
+++ src/main.cpp
@@ -12,31 +12,6 @@ int WINAPI WinMain(HINSTANCE i, HINSTANCE, LPSTR, int)
 {
 	char const * const class_name = "MAINFRAME";
 
-	std::vector<font::EnumFontInfo> ff;
-	// font::list_fonts(ff, ANSI_CHARSET, true);
-	font::list_fonts(ff, DEFAULT_CHARSET);
-
-	printf("\n%d fonts found\n", ff.size());
-
-	for(size_t i=0; i<ff.size(); ++i)
-	{
-		char const * ft = "?";
-		switch(ff[i].FontType)
-		{
-			case DEVICE_FONTTYPE: ft = "dev"; break;
-			case RASTER_FONTTYPE: ft = "ras"; break;
-			case TRUETYPE_FONTTYPE: ft = "ttf"; break;
-		};
-
-		printf(" %3d | %s | %s | %s | %d | %s\n", i+1, ft,
-			ff[i].elfe.elfLogFont.lfFaceName,
-			ff[i].elfe.elfStyle,
-			ff[i].elfe.elfLogFont.lfCharSet,
-			ff[i].elfe.elfScript);
-	}
-
-	return 0;
-
 	window::create_class(class_name, MainFrameProc, i);
 	HWND frame = window::create_frame(class_name, i);
 	SetWindowText(frame, "monotide-redux");
@@ -50,11 +25,35 @@ int WINAPI WinMain(HINSTANCE i, HINSTANCE, LPSTR, int)
 	return msg.wParam;
 }
 
+void draw_fonts(HDC, std::vector<font::EnumFontInfo> &, size_t=0);
+
 LRESULT CALLBACK MainFrameProc(HWND h, UINT m, WPARAM w, LPARAM l)
 {
+	static std::vector<font::EnumFontInfo> ff;
+	static size_t ff_skipped = 0;
+
 	switch(m)
 	{
 		default: break;
+
+		case WM_PAINT:
+		{
+			PAINTSTRUCT ps;
+			BeginPaint(h, &ps);
+			draw_fonts(ps.hdc, ff, ff_skipped);
+			EndPaint(h, &ps);
+			return 0;
+		}
+
+		case WM_CREATE:
+		{
+			HDC dc = GetDC(h);
+			font::list_fonts(ff, ANSI_CHARSET, true, dc);
+			ReleaseDC(h, dc);
+
+			printf(" %d fonts found\n", ff.size());
+			return 0;
+		}
 
 		case WM_KEYDOWN:
 		{
@@ -81,4 +80,37 @@ LRESULT CALLBACK MainFrameProc(HWND h, UINT m, WPARAM w, LPARAM l)
 			return 0;
 	}
 	return DefWindowProc(h, m, w, l);
+}
+
+void draw_fonts(HDC dc, std::vector<font::EnumFontInfo> & ff, size_t skip)
+{
+	font::list_fonts(ff, ANSI_CHARSET, true, dc);
+
+	unsigned const margin = 8;
+	HWND const parent = WindowFromDC(dc);
+	RECT cr;
+	GetClientRect(parent, &cr);
+	unsigned y = 0;
+
+	for (size_t i=skip; i<ff.size(); ++i)
+	{
+		if (y + ff[i].elfe.elfLogFont.lfHeight > cr.bottom - margin)
+			break;
+
+		// printf(" %3d | %s | %s | %s | %d | %s\n", i+1, ft,
+		// 	ff[i].elfe.elfLogFont.lfFaceName,
+		// 	ff[i].elfe.elfStyle,
+		// 	ff[i].elfe.elfLogFont.lfCharSet,
+		// 	ff[i].elfe.elfScript);
+
+		char const * text = (char const *) ff[i].elfe.elfFullName;
+
+		HFONT hf = CreateFontIndirect(&ff[i].elfe.elfLogFont);
+		HGDIOBJ old = SelectObject(dc, hf);
+		TextOut(dc, margin, margin + y, text, strlen(text));
+		SelectObject(dc, old);
+		DeleteObject(hf);
+
+		y += ff[i].elfe.elfLogFont.lfHeight;
+	}
 }
