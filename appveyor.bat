dfb3db0fd3e603856c52b78d5ed1ed7f3108e980
Dejan Budimir <dejbug@users.noreply.github.com>
2017-06-05 01:58:12 +0200

    still tinkering with appveyor

---
+++ appveyor.bat
@@ -0,0 +1,74 @@
+@ECHO off
+
+REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM
+:prepare
+
+ECHO -- [ running ] "stage: prepare"
+
+ECHO -- setting MINGW paths
+SET MINGW_BIN=C:\mingw-w64\i686-5.3.0-posix-dwarf-rt_v4-rev0\mingw32\bin
+SET PATH=%MINGW_BIN%;%PATH%
+
+ECHO -- renaming mingw32-make.exe in-place
+PUSHD %MINGW_BIN%
+RENAME mingw32-make.exe make.exe 
+POPD
+
+ECHO -- [ ready ] for make
+
+
+REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM
+:build
+
+ECHO -- [ running ] "stage: build"
+
+ECHO -- selecting build target
+
+IF "%APPVEYOR_REPO_TAG%"=="true" GOTO build_release
+IF "%APPVEYOR_REPO_TAG%"=="false" GOTO build_debug
+GOTO :EOF
+
+
+REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM
+:build_debug
+ECHO -- building for debug
+make
+
+ECHO -- cleaning up build folder
+DEL build\*.o
+
+GOTO :EOF
+
+
+REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM
+:build_release
+ECHO -- building for release
+make TARGET=release
+
+ECHO -- cleaning up build folder
+DEL build\*.o
+REM TODO: add a "release.url" linking to the deployed GitHub release URL.
+REM ECHO [InternetShortcut] > release.url
+REM ECHO URL=%GITHUB_RELEASE_URL% >> release.url
+
+ECHO -- packing output to deploy
+MKDIR deploy
+7z a deploy\monotide-%APPVEYOR_REPO_TAG_NAME%.zip build\*
+
+GOTO :EOF
+
+
+REM ------ 8< -------- ( snip ) ---------- 8< -------- ( snip ) -------
+GOTO :EOF
+
+
+REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM
+:ok
+ECHO -- [ OK ] build
+GOTO :EOF
+
+
+REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM
+:failed
+ECHO -- [ FAILED ] build
+GOTO :EOF
--- scripts/appveyor-build-leave.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo -- [ running ] "appveyor post-build script"
-
-echo -- cleaning up deployment folder
-del build\*.o
-
-echo -- ready for deployment
